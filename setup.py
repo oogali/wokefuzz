bf3ce3475fdeeaf9909d7a8078a6dfea23b477ff
Omachonu Ogali <oogali@gmail.com>
2023-09-23 07:36:36 -0400

    Fix permissions and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name="wokefuzz",
-        version="0.0.1",
+        version="0.0.2",
         python_requires=">=3.7, <4",
         install_requires=[
             'woke==3.5.0',
